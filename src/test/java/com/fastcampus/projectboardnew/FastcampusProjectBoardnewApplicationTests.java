dbb25480c27bf08d5179baa2b275a33ebf127166
eunyoung go <unn3290@gmail.com>
2023-08-22 23:23:18 +0900

    #7-스프링 부트 프로젝트 시
    *스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ src/test/java/com/fastcampus/projectboardnew/FastcampusProjectBoardnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboardnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusProjectBoardnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
